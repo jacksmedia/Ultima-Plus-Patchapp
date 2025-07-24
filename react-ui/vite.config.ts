5c17ba93a26380f4e48f17191f3192868bd2a691
jacksmedia <alex@j4cks.com>
2025-07-24 14:51:33 -0700

    🎢rm React boilerplate

--- react-ui/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-// import { VitePWA } from 'vite-plugin-pwa'
-
-export default defineConfig({
-  plugins: [
-    reactRouter()
-  ]
-}); 
